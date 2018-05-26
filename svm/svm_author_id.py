4e1fb6b131791395f2c15d8476482e537266ce54
Omkar Chaporkar <omkar.chaporkar230@gmail.com>
2018-05-27 02:37:09 +0530

    Add files via upload
    updated

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,3 +1,4 @@
+# -*- coding: cp1252 -*-
 #!/usr/bin/python
 
 """ 
@@ -7,7 +8,8 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
+   
 import sys
 from time import time
 sys.path.append("../tools/")
@@ -24,7 +26,34 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import svm
+clf = svm.SVC(C=10000.0, kernel='rbf')
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+clf.fit(features_train,labels_train)
 
+pred = clf.predict(features_test)
+from sklearn.metrics import accuracy_score
+
+accuracy = accuracy_score(pred,labels_test)
+print accuracy
+
+ans10 = pred[50]
+print ans10
+
+#total 1700 test events
+#How many are predicted to be in Chris or [1] class??
+
+chris=0
+var=0
+for i in range(1758):
+    var = pred[i]
+    if (var == 1):
+        chris+=1
+    else:
+        pass
+
+print chris
 #########################################################
 
 
